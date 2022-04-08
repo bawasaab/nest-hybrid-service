2352bd50cb79f9bfe09ea5dbe2b820ba7c46642a
DESKTOP-B421EOF\admin <deepak.bawa@orionesolutions.in>
2022-04-08 17:43:31 +0530

    hybrid transporters

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserEvent } from './events/create-user.event';
 
 @Injectable()
 export class AppService {
+  private readonly analytics: any[] = [];
   getHello(): string {
     return 'Hello World!';
+  }
+
+  handleUserCreated(data: CreateUserEvent) {
+    console.log(
+      'an user_created event emitted from api-gateway to analytics in handleUserCreated hybrid app',
+      data,
+    );
+    this.analytics.push({
+      emai: data.email,
+      timestamp: new Date(),
+    });
+    console.log('analytics', this.analytics);
   }
 }
